ed7e37f4d528ad3c4dd7ed1b87bf378267f4526d
vancemd1 <47529569+vancemd1@users.noreply.github.com>
2021-01-04 13:24:01 -0500

    basic bot functionality

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
-print("Hello world but from the right branch this time")
+import sys
+import discord
+try:
+	token = sys.argv[1]
+except IndexError:
+	print("Didn't get a token! Run \"py bot.py <token here>\" to launch the bot.")
+	sys.exit(1) #exit with error, we don't need to do anything else w/o a token
+
+client = discord.Client() #making a new client object
+
+@client.event
+async def on_ready():
+	print("Bot connected as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+	if message.content.startswith("!test"):
+		await message.channel.send("Hello world")
+		print("Sent response, exiting.")
+
+
+client.run(token)
+
